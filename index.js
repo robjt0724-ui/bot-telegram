49d1ef1a8fa183ad5b9de4246a8adf49e0a2d912
robjt0724-ui <robjt0724@gmail.com>
2026-04-25 17:40:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,42 +2,14 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const token = process.env.TOKEN;
 
-const bot = new TelegramBot(token, { polling: true });
-
-const grupoA = -1002110613167;
-const grupoB = -1001831601806;
-
-bot.on('message', async (msg) => {
-  if (msg.chat.id !== grupoA) return;
-
-  try {
-    // TEXTO
-    if (msg.text) {
-      await bot.sendMessage(grupoB, msg.text);
-    }
-
-    // FOTO
-    if (msg.photo) {
-      const fileId = msg.photo[msg.photo.length - 1].file_id;
-      await bot.sendPhoto(grupoB, fileId, { caption: msg.caption || '' });
-    }
-
-    // VÍDEO
-    if (msg.video) {
-      await bot.sendVideo(grupoB, msg.video.file_id, { caption: msg.caption || '' });
-    }
-
-    // ÁUDIO
-    if (msg.audio) {
-      await bot.sendAudio(grupoB, msg.audio.file_id);
-    }
-
-    // DOCUMENTO
-    if (msg.document) {
-      await bot.sendDocument(grupoB, msg.document.file_id);
-    }
-
-  } catch (error) {
-    console.log('Erro:', error.message);
+// cria bot
+const bot = new TelegramBot(token, {
+  polling: {
+    autoStart: false
   }
+});
+
+// reinicia polling limpo
+bot.stopPolling().then(() => {
+  bot.startPolling();
 });
