0c4dd3b18d9c5ef8e75f0f27f8c83a44783f921e
robjt0724-ui <robjt0724@gmail.com>
2026-04-25 18:05:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-const TelegramBot = require('node-telegram-bot-api');
-
-const token = process.env.TOKEN;
-
-// NÃO inicia polling
-const bot = new TelegramBot(token, { polling: false });
-
-console.log("BOT EM MODO STOP (limpando sessão)");
+const bot = new TelegramBot(token, { polling: true });
