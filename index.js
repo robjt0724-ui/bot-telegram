ce12f76a3b4365e370bf50854f90ad89acda7aba
robjt0724-ui <robjt0724@gmail.com>
2026-04-25 17:55:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = process.env.TOKEN;
-const bot = new TelegramBot(token, { polling: true });
 
-// GRUPOS
-const grupoC = -1003906204677;
-const grupoA = -1002110613167;
-const grupoB = -1001831601806;
-
-// MENSAGEM PADRÃO
-const legendaPadrao = `🔥🎬 TEM FILME BOM TE ESPERANDO AGORA! 🍿✨
-
-Aquele tipo de filme que te prende do começo ao fim 😱💥
-
-📲 Quer assistir agora? Me chama no WhatsApp 👇💬
-
-https://wa.link/e8t534`;
-
-// FILA DE IMAGENS
-let fila = [];
-
-// CAPTURA DO GRUPO C
-bot.on('message', (msg) => {
-  if (msg.chat.id !== grupoC) return;
-
-  if (msg.photo) {
-    const fileId = msg.photo[msg.photo.length - 1].file_id;
-    fila.push(fileId);
-  }
+// força sem polling automático
+const bot = new TelegramBot(token, {
+  polling: false
 });
 
-// ENVIO A CADA 45 MINUTOS
-setInterval(async () => {
-  if (fila.length === 0) return;
-
-  const imagem = fila.shift();
-
-  try {
-    await bot.sendPhoto(grupoA, imagem, { caption: legendaPadrao });
-    await bot.sendPhoto(grupoB, imagem, { caption: legendaPadrao });
-
-    console.log("Enviado para A e B");
-  } catch (err) {
-    console.log("Erro:", err.message);
-  }
-
-}, 45 * 60 * 1000);
+console.log("BOT INICIADO SEM POLLING");
