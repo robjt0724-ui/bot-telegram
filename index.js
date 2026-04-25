448b93994adc6bf7c2ce972d36d21f0d39b69da2
robjt0724-ui <robjt0724@gmail.com>
2026-04-25 16:24:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const token = process.env.TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
+// COLE SEUS IDs AQUI
+const grupoA = -1002110613167;
+const grupoB = -1001831601806;
+
 bot.on('message', (msg) => {
-  console.log(msg.chat.id);
+  if (msg.chat.id === grupoA) {
+    bot.forwardMessage(grupoB, grupoA, msg.message_id);
+  }
 });
