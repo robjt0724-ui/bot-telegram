a79aedb52c1bf41fee835d78831d529879a4d534
robjt0724-ui <robjt0724@gmail.com>
2026-04-25 18:06:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const bot = new TelegramBot(token, { polling: true });
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.TOKEN;
+
+const bot = new TelegramBot(token, {
+  polling: true
+});
+
+console.log("BOT INICIADO COM SUCESSO");
+
+bot.on('message', (msg) => {
+  console.log("Mensagem recebida:", msg.text || "[mídia]");
+});
